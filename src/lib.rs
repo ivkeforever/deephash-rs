cab7009a9bcf2b2570754e490d1aff2ba3a24d12
ivkeforever <ivke11080@gmail.com>
2022-09-13 10:12:32 +0200

    ui update and button logic

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ use eframe::{
     Frame,
 };
 
-const HASH_FIELD_WIDTH: f32 = 380.0;
+const HASH_FIELD_WIDTH: f32 = 440.0;
 const LABEL_TO_INPUT_SPACE: f32 = 10.0;
 const LABEL_TEXT_SIZE: f32 = 16.0;
 
 pub struct HashApp {
-    picked_file: Option<String>,
+    picked_file: String,
     md5_hash: String,
     sha1_hash: String,
     sha256_hash: String,
@@ -17,19 +17,33 @@ pub struct HashApp {
 impl Default for HashApp {
     fn default() -> Self {
         Self {
-            picked_file: None,
+            picked_file: String::new(),
             md5_hash: String::new(),
             sha1_hash: String::new(),
             sha256_hash: String::new(),
         }
     }
 }
-
+// TODO: Eliminate all hardcoded values and see if sizes are consistent across different machines
 impl eframe::App for HashApp {
-    fn update(&mut self, ctx: &Context, frame: &mut Frame) {
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
         egui::CentralPanel::default().show(ctx, |ui: &mut Ui| {
             ui.heading(egui::RichText::new("Calculate Hashes").size(30f32));
-            ui.add_space(30f32);
+            ui.add_space(20f32);
+            ui.horizontal(|ui| {
+                ui.add(egui::Label::new(egui::RichText::new("File: ")
+                    .size(LABEL_TEXT_SIZE)));
+                ui.add_space(LABEL_TO_INPUT_SPACE + 22.0);
+                ui.add_sized([HASH_FIELD_WIDTH-68.0, 20.0], egui::TextEdit::singleline(&mut self.picked_file)
+                                .interactive(false)
+                );
+                if ui.button("Open file").clicked() {
+                    if let Some(path) = rfd::FileDialog::new().pick_file() {
+                        self.picked_file = path.display().to_string();
+                    }
+                }
+            });
+            ui.add_space(20f32);
             ui.horizontal(|ui| {
                 ui.add(egui::Label::new(
                     // HARDCODED SPACE INSIDE RICHTEXT, MAYBE FIX THIS (FOR EVERY HASH)
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ use file_hasher_rs::HashApp;
 fn main() {
     let options = eframe::NativeOptions {
         drag_and_drop_support: true,
+        initial_window_size: Some(egui::Vec2::new(540.0, 320.0)),
+        resizable: false,
         ..Default::default()
     };
 
     eframe::run_native(
-        "Rust File Hasher (v1.0.1)",
+        "DeepHash (v1.0.1)",
         options,
         Box::new(|_cc| Box::new(HashApp::default())),
     );
